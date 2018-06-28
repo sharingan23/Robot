de09bf839240921ed799c08973d7ed90cb785f99
sugeethkumar <sugeethpro@hotmail.com>
2018-06-28 13:45:26 +0200

    etape 4 de l'exercice , done.

---
+++ Robot/Robot.swift
@@ -0,0 +1,29 @@
+//
+//  Robot.swift
+//  Robot
+//
+//  Created by Vigneswaranathan Sugeethkumar on 26/06/2018.
+//  Copyright © 2018 Vigneswaranathan Sugeethkumar. All rights reserved.
+//
+
+import Foundation
+
+class Robot {
+    var name = ""
+    var hp = 0
+    var moveSpeed = 0
+    var position : [Int]
+    
+    init (){
+        hp = 100
+        moveSpeed = 3
+        position = [0,0]
+    }
+    
+    func introduce() {
+        print("Bonjour je m'apelle "+name+" . j'ai \(hp) points de vie et je me deplace à 3 cases par secondes. Je suis à la case de coordonnées(\(position[0]);\(position[1]))")
+    }
+    
+}
+
+
--- Robot/main.swift
+++ Robot/main.swift
@@ -2,11 +2,37 @@
 //  main.swift
 //  Robot
 //
-//  Created by Vigneswaranathan Sugeethkumar on 28/06/2018.
+//  Created by Vigneswaranathan Sugeethkumar on 26/06/2018.
 //  Copyright © 2018 Vigneswaranathan Sugeethkumar. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+var j23 = Robot()
+j23.name = "j23"
 
+var s23 = Robot()
+s23.name = "s23"
+
+s23.introduce()
+
+j23.introduce()
+
+var j5 = Robot()
+j5.name = "j5"
+
+var s5 = Robot()
+s5.name = "s5"
+
+var manyRobot : [Robot]
+
+manyRobot = [j23,j5,s23,s5]
+
+var i = 0
+var j = manyRobot.count
+
+
+while i < j {
+    manyRobot[i].introduce()
+i+=1
+}
