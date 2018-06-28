31b9b8fcb66a6a0cb494ab06821c1fd93a540d27
sugeethkumar <sugeethpro@hotmail.com>
2018-06-28 13:43:21 +0200

    Initial Commit

---
+++ Robot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Robot
+//
+//  Created by Vigneswaranathan Sugeethkumar on 28/06/2018.
+//  Copyright © 2018 Vigneswaranathan Sugeethkumar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
